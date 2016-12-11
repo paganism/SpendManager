5096e4bbc8ec618f27f75b1796baf51fda6c9269
GreenCaterpillar <1k1g1b@mail.ru>
2016-12-11 16:42:53 +0300

    test #2

--- src/com/caterhog/Main.java
+++ src/com/caterhog/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         //test от мурлыки
+        //test #2
         Locale.setDefault(Locale.ENGLISH);
         //Income ins = new Income();
         //ins.incomeIns(500, "Не Мурлычьи деньги");
